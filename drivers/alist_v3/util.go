c9f4ab0ec4b038b030b09b28ad12731a989948e9
ImoutoHeaven <github_main@imoutoheaven.org>
2025-04-09 10:38:52 +0800

    Update util.go

--- drivers/alist_v3/util.go
+++ drivers/alist_v3/util.go
@@ -33,33 +33,57 @@ func (d *AListV3) login() error {
 
 func (d *AListV3) request(api, method string, callback base.ReqCallback, retry ...bool) ([]byte, int, error) {
 	url := d.Address + "/api" + api
+	log.Debugf("[alist_v3] Making %s request to: %s", method, url)
+	
 	req := base.RestyClient.R()
 	req.SetHeader("Authorization", d.Token)
+	log.Debugf("[alist_v3] Using authorization token: %s", d.Token)
+	
 	if callback != nil {
+		log.Debug("[alist_v3] Executing callback function")
 		callback(req)
 	}
+	
+	log.Debugf("[alist_v3] Executing request: %s %s", method, url)
 	res, err := req.Execute(method, url)
+	
 	if err != nil {
 		code := 0
 		if res != nil {
 			code = res.StatusCode()
 		}
+		log.Debugf("[alist_v3] Request error: %v, status code: %d", err, code)
 		return nil, code, err
 	}
-	log.Debugf("[alist_v3] response body: %s", res.String())
+	
+	log.Debugf("[alist_v3] Response status: %s", res.Status())
+	log.Debugf("[alist_v3] Response body: %s", res.String())
+	
 	if res.StatusCode() >= 400 {
+		log.Debugf("[alist_v3] HTTP error status: %s", res.Status())
 		return nil, res.StatusCode(), fmt.Errorf("request failed, status: %s", res.Status())
 	}
+	
 	code := utils.Json.Get(res.Body(), "code").ToInt()
+	log.Debugf("[alist_v3] Response code from body: %d", code)
+	
 	if code != 200 {
+		message := utils.Json.Get(res.Body(), "message").ToString()
+		log.Debugf("[alist_v3] Request failed with code: %d, message: %s", code, message)
+		
 		if (code == 401 || code == 403) && !utils.IsBool(retry...) {
+			log.Debug("[alist_v3] Authentication error, attempting to re-login")
 			err = d.login()
 			if err != nil {
+				log.Debugf("[alist_v3] Re-login failed: %v", err)
 				return nil, code, err
 			}
+			log.Debug("[alist_v3] Re-login successful, retrying request")
 			return d.request(api, method, callback, true)
 		}
-		return nil, code, fmt.Errorf("request failed,code: %d, message: %s", code, utils.Json.Get(res.Body(), "message").ToString())
+		return nil, code, fmt.Errorf("request failed, code: %d, message: %s", code, message)
 	}
+	
+	log.Debug("[alist_v3] Request completed successfully")
 	return res.Body(), 200, nil
 }
